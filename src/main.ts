18e4ec72a83b19febcdb65722713cb525520d226
dpalexandru <alexpoian91@gmail.com>
2025-12-17 11:59:14 +0100

    [Snack1] aggiunti controlli su null, array e promise

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 
 let dato: unknown;
 
-dato = [];
+dato = [1,2,3];
 
 
 // se è una stringa stampo in maiuscolo
@@ -12,7 +12,13 @@ if (typeof dato === 'string') {
 console.log(dato * 2)
 } else if(typeof dato === 'boolean'){
   console.log(dato? "si ": "no")
-}  else {
+} else if(dato === null){
+ console.log("Il dato è vuoto")
+} else if(Array.isArray(dato )) {
+console.log(dato.length)
+} else if (dato instanceof Promise) {
+dato.then( msg => console.log(msg))
+} else {
   console.log("Dato non supportato")
 }
 
