b7c4b468dbf6aedcb570fc1aab1a77519adff097
dpalexandru <alexpoian91@gmail.com>
2025-12-17 12:04:09 +0100

    [Snack2] creazione di type dipendene

--- src/main.ts
+++ src/main.ts
@@ -24,5 +24,24 @@ dato.then( msg => console.log(msg))
 
 
 
+// Snack 2 
+// Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+
+// nome → stringa
+// cognome → stringa
+// annoNascita → numero
+// sesso → Può essere solo "m" o "f".
+// anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: "m" | "f",
+  anniDiServizio: number[];
+
+}
+
+
 
 
